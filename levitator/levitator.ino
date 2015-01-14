0d7f5664c18143c8d621919a228b116672b2f795
fatcloud <victor19871226@hotmail.com>
2015-01-14 22:41:09 +0800

    very slow but stable position sensing

--- levitator/levitator.ino
+++ levitator/levitator.ino
@@ -6,28 +6,37 @@ void setup() {
   pinMode(LASER_OUT, OUTPUT );
 }
 
-const int halfPeriod = 25;  // 1/60 sec + 4 millisecond
+const int halfPeriod = 29;  // 1/60 sec + 4 millisecond
 int  onTime = 0, offTime = 0;
 int  localMax = 0, localMin = 1024, localAvg = 0;
+
+int  avgHi = 0, avgLo = 0;
+boolean  prev_laser_on, laser_on;
+
 void loop() { 
-  boolean laser_on = ( ( millis() % halfPeriod * 2 ) > ( halfPeriod - 1 ) );
+  prev_laser_on = laser_on;
+  laser_on = ( ( millis() % halfPeriod * 2 ) > ( halfPeriod - 1 ) );
   if( laser_on )  onTime = millis();
   else  offTime = millis();
   digitalWrite(LASER_OUT, laser_on );
 
   int val = analogRead( ANALOG_IN );  
-  if( abs(onTime - offTime) > 5 ) { 
+  if( abs(onTime - offTime) > 6 ) { 
     if( val > localMax ){ localMax = val; }
     if( val < localMin ){ localMin = val; }
     localAvg = ( localMax + localMin ) / 2;
   } else {
     localMax = 0;
     localMin = 1023;
-    val = localAvg;
+    if( laser_on ) avgLo = localAvg;
+    else avgHi = localAvg;
+    val = avgHi - avgLo;
   }
   
-  if( val < 0 ){ val = 0; }
-  Serial.write( 0xff );
-  Serial.write( (val >> 8) & 0xff );
-  Serial.write( val & 0xff );
+  if( prev_laser_on != laser_on ) {
+    if( val < 0 ){ val = 0; }
+    Serial.write( 0xff );
+    Serial.write( (val >> 8) & 0xff );
+    Serial.write( val & 0xff );
+  }
 }
